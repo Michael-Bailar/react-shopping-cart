9ede92c7b0e99abd3604707161111bc2c90ab2f0
Michael-B1764 <mikebailar@gmail.com>
2020-03-19 11:39:42 -0700

    done with mvp. Will work on converting some other projects to context api

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Item = props => {
+	console.log('cart props', props)
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
